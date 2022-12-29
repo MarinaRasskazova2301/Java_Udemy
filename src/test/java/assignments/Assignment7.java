1b9c51806f45a2dc008966dff1b19009b71ed5e6
Maryna Rasskazova <marinarasskazova2301@gmail.com>
2022-12-29 20:09:22 +0200

    Assignment7 updated

--- src/test/java/assignments/Assignment7.java
+++ src/test/java/assignments/Assignment7.java
@@ -16,17 +16,14 @@ public class Assignment7 {
 
         System.out.println("There are " + table.findElements(By.tagName("tr")).size() + " rows in table");
         System.out.println("There are " + table.findElements(By.cssSelector("tr th")).size() + " columns in table");
-        System.out.println("The text in 2d row is: " );
-       // System.out.println(table.findElement(By.cssSelector("tr:nth-child(3)")).getText());
+        System.out.println("The text in 2d row is: ");
+        // System.out.println(table.findElement(By.cssSelector("tr:nth-child(3)")).getText());
 
-        String rowText=null;
-
-        for (WebElement cell: table.findElements(By.tagName("tr")).get(2).findElements(By.tagName("td"))
-             ) {
-            System.out.print(cell.getText()+" | ");
+        for (WebElement cell : table.findElements(By.tagName("tr")).get(2).findElements(By.tagName("td"))
+        ) {
+            System.out.print(cell.getText() + " | ");
 
         }
-
 
         driver.quit();
 
